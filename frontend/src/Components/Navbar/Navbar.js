c5d96bfb88acb3086f9c9aea81073c0abb178e5c
sunilskcj <sunilskcj@gmail.com>
2023-09-29 20:44:00 +0530

    commit issue solved

---
+++ frontend/src/Components/Navbar/Navbar.js
@@ -0,0 +1,34 @@
+import './Navbar.css';
+function Navbar(){
+    return(
+        <nav class="navbar navbar-expand-lg ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Sunil kumar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    </div>
+    <div>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul> 
+    </div>
+  </div>
+</nav>
+        
+       );
+}
+
+export default Navbar;
