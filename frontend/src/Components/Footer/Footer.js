d233d7699f416e600a43a086b03d14fd5dbe263d
sunilskcj <sunilskcj@gmail.com>
2024-01-03 21:28:16 +0530

    random

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -6,9 +6,7 @@ return(
        <div><h3>Sunil</h3></div> 
        
        <div>Social <p>Instagram</p><p>Gmail</p></div>
-       <div> <h5>Social</h5> <p>Instagram</p><p>Gmail</p></div>
-       <div> <h5>Social</h5> <p>Instagram</p><p>Gmail</p></div>
-      
+       
     </footer>
 );
 }
