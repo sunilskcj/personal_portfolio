5ab04e0f58c5e48956af027aa53c67b8a5fe3ac4
sunilskcj <sunilskcj@gmail.com>
2024-01-04 21:37:46 +0530

    random 1

--- frontend/src/Components/About/About.js
+++ frontend/src/Components/About/About.js
@@ -1,6 +1,6 @@
 function About() {
     return(<about>
-        about
+        
 
         </about>);
 }
--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
-function Navbar(){
+function Navbar(){  
     return(
         <nav class="navbar navbar-expand-lg ">
   <div class="container-fluid">
