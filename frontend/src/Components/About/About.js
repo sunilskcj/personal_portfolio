52b93347e080cc65ec422ba897fac9ebfed09361
sunilskcj <sunilskcj@gmail.com>
2024-01-20 12:54:06 +0530

    about v0

---
+++ frontend/src/Components/About/About.js
@@ -0,0 +1,90 @@
+import './About.css'
+function About(){
+    return(
+        <div id='paper'>
+        <div id='head'> 
+            
+        
+            
+        </div>
+        <hr className='solid' />
+        <div >
+        <table id="Ta" >
+        <tr>
+        <td id='p0'>CAREER OBJECTIVE</td>
+        <td id='p-p0'>A curious learner and excellent team player searching for a challenging position as a Software Engineer with 2 years of experience to utilize my technical competencies, working knowledge of software development, troubleshooting and networking services and excellent communication skills to grow as a Software Engineer.</td>
+        </tr>       
+        <tr>
+        <td id='p0'>QUALIFICATION</td>
+        <td id='p-p0'>B.E, Electrical and Electronic Engineering (2017-2021) Panimalar Enginering  College, CGPA – 7.92</td>
+        </tr>
+        <tr>
+        <td id='p0'>HSC</td>
+        <td id='p-p0'>Sri Gnanananda Matric. Hr. Sec. School, Academic Year: 2016 – 2017 (74 %)</td>
+        </tr>
+        <tr>
+        <td id='p0'>SSLC  </td>
+        <td id='p-p0'>Sri Ramachandra Vidhyalaya High School, Academic Year: 2015 – 2016 (89 %)</td>
+        </tr>
+        <tr>
+        <td id='p0'>AREA OF INTEREST </td>
+        <td id='p-p0'><ul >
+        <li>Web Development.</li>
+        <li>Java Developer</li>
+        <li>Object Oriented Programming</li>
+        </ul></td>
+        </tr>
+        <tr>
+        <td id='p0'>CERTIFICATION </td>
+        <td id='p-p0'>Java Full Stack Developer, React JS </td>
+        </tr>
+        <tr>
+        <td id='p2'>TECHNICAL SKILLS </td>
+        <td id='p-p0'> <ul>
+            <li className='li_head'>FULL STACK WEBDELOPMENT <ul>
+                <li className='li_head2' >Proficient in React.JS<ul>
+                    <li className='li_head3'>HTML, CSS, JavaScript</li>
+                    <li className='li_head3'>Database : MYSQL</li>
+                    </ul>
+                    </li>
+                
+                </ul></li>
+                                                                
+            </ul></td>
+        
+        </tr>
+       
+        <tr>
+        <td id='p0'>INTERPERSONAL SKILLS  </td>
+        <td id='p-p0'><ul >
+        <li>	Quick Learner.</li>
+        <li>Positive can-do attitude.</li>
+        <li>Able to adapt myself to the environment</li>
+        </ul></td>
+        </tr>
+     
+        <tr>
+        <td id='p0'>HOBBIES </td>
+        <td id='p-p0'><ul>
+        <li>UI/UX Designing</li>
+        <li>blogging about tech.</li>
+        <li>Building Custom PCs.</li>
+        </ul></td>
+        </tr>
+        <tr>
+        <td id='p0'>PERSONAL DETAILS  </td>
+        <td id='p-p0'><ul  >
+        <li>DOB :07-07-2000 </li>
+        <li>Languages Known : Tamil, English</li>
+        <li>Address :No:23, Block A,
+        Ayyanar nagar,
+        Ariyur,
+        Puducherry - 605102.</li>
+        </ul></td>
+        </tr>
+        </table>
+        </div><br /><br />
+    </div>);
+}
+
+export default About;
